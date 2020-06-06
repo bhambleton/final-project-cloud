eb5451495aae6f9d79c46b9a9ec093ff5cebdad2
ThuyVy Nguyen <thuyvynguy@gmail.com>
2020-06-06 12:16:50 -0700

    get all courses, get specific course, delete specific course, and post course all done!

--- api/courses.js
+++ api/courses.js
@@ -1,10 +1,143 @@
 const router = require('express').Router();
+const { validateAgainstSchema } = require('../lib/validation');
+const { generateAuthToken, requireAuthentication, checkAuthentication } = require('../lib/auth');
+const {
+    CourseSchema,
+    insertNewCourse,
+    getCourseById,
+    getCourseStudentsById,
+    getCourseAssignmentsById,
+    getCoursesPage,
+    deleteCourseById
+} = require('../models/course');
 
-router.post('/', function (req, res, next) {
-    res.status(200).send({
-        valid: "Valid"
-    })
+//Get all courses
+router.get('/', async (req, res) => {
+    try {
+        /*
+         * Fetch page info, generate HATEOAS links for surrounding pages and then
+         * send response.
+         */
+        const CoursePage = await getCoursesPage(parseInt(req.query.page) || 1);
+        CoursePage.links = {};
+        if (CoursePage.page < CoursePage.totalPages) {
+            CoursePage.links.nextPage = `/Courses?page=${CoursePage.page + 1}`;
+            CoursePage.links.lastPage = `/Courses?page=${CoursePage.totalPages}`;
+        }
+        if (CoursePage.page > 1) {
+            CoursePage.links.prevPage = `/Courses?page=${CoursePage.page - 1}`;
+            CoursePage.links.firstPage = '/Courses?page=1';
+        }
+        res.status(200).send(CoursePage);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({
+            error: "Error fetching Courses list.  Please try again later."
+        });
+    }
+});
 
+// Inserting New Course
+router.post('/', checkAuthentication, async (req, res) => {
+    if (req.role == 'admin'){
+        if (validateAgainstSchema(req.body, CourseSchema)) {
+            try {
+                const id = await insertNewCourse(req.body);
+                res.status(201).send({
+                    id: id,
+                    links: {
+                        Course: `/Courses/${id}`
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({
+                    error: "Error inserting Course into DB.  Please try again later."
+                });
+            }
+        } else {
+            res.status(400).send({
+                error: "Request body is not a valid Course object."
+            });
+        }
+    }else{
+        res.status(403).send({
+            error: "Invalid permissions."
+        });
+
+    }
+});
+
+// router.get('/:id/students', async (req, res, next) => {
+//     try {
+//         const Course = await getCourseStudentsById(req.params.id);
+//         if (Course) {
+//             res.status(200).send(Course);
+//         } else {
+//             next();
+//         }
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send({
+//             error: "Unable to fetch Course's students.  Please try again later."
+//         });
+//     }
+// });
+
+// router.get('/:id/assignments', async (req, res, next) => {
+//     try {
+//         const Course = await getCourseAssignmentsById(req.params.id);
+//         if (Course) {
+//             res.status(200).send(Course);
+//         } else {
+//             next();
+//         }
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send({
+//             error: "Unable to fetch Course's assignments.  Please try again later."
+//         });
+//     }
+// });
+
+// Fetch Info about specific course (w/out assignments & students enrolled)
+router.get('/:id', async (req, res, next) => {
+    try {
+        const Course = await getCourseById(req.params.id);
+        if (Course) {
+            res.status(200).send(Course);
+        } else {
+            next();
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({
+            error: "Unable to fetch Course.  Please try again later."
+        });
+    }
+});
+
+router.delete('/:id', checkAuthentication, async (req, res,next) => {
+    if (req.role == 'admin') {
+        try{
+            const deleteSuccessful = await deleteCourseById(req.params.id);
+            if (deleteSuccessful){
+                res.status(204).end();
+            }else{
+                next();
+            }
+        } catch (err){
+            console.log(err);
+            res.status(500).send({
+                error: "Unable to delete course"
+            })
+        }
+    } else {
+        res.status(403).send({
+            error: "Invalid permissions."
+        });
+
+    }
 });
 
 
---
+++ models/course.js
@@ -0,0 +1,135 @@
+const { ObjectId } = require('mongodb');
+
+const { getDBReference } = require('../lib/mongo');
+const { extractValidFields } = require('../lib/validation');
+// Get student by Id :) const { getStudentByCourseId } = require('./student');
+// Get assignment by Id :) const { getAssignmentyCourseId } = require('./assignment');
+
+const CourseSchema = {
+    subject: { required: true },
+    number: { required: true },
+    title: { required: true },
+    term: { required: true },
+    instructorID: { required: true }
+};
+exports.CourseSchema = CourseSchema;
+
+async function getCoursesPage(page) {
+    const db = getDBReference();
+    const collection = db.collection('Courses');
+    const count = await collection.countDocuments();
+
+    /*
+     * Compute last page number and make sure page is within allowed bounds.
+     * Compute offset into collection.
+     */
+    const pageSize = 10;
+    const lastPage = Math.ceil(count / pageSize);
+    page = page > lastPage ? lastPage : page;
+    page = page < 1 ? 1 : page;
+    const offset = (page - 1) * pageSize;
+
+    const results = await collection.find({})
+        .sort({ _id: 1 })
+        .skip(offset)
+        .limit(pageSize)
+        .toArray();
+
+    return {
+        Courses: results,
+        page: page,
+        totalPages: lastPage,
+        pageSize: pageSize,
+        count: count
+    };
+}
+exports.getCoursesPage = getCoursesPage;
+
+async function insertNewCourse(course) {
+    course = extractValidFields(course, CourseSchema);
+    const db = getDBReference();
+    const collection = db.collection('Courses');
+    const result = await collection.insertOne(course);
+    return result.insertedId;
+}
+exports.insertNewCourse = insertNewCourse;
+
+async function getCourseById(id) {
+    const db = getDBReference();
+    const collection = db.collection('Courses');
+    if (!ObjectId.isValid(id)) {
+        return null;
+    } else {
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .toArray();
+        return results[0];
+    }
+}
+exports.getCourseById = getCourseById;
+
+async function getCourseStudentsById(id) {
+    /*
+     * Execute three sequential queries to get all of the info about the
+     * specified Course, including its photos.
+     */
+    const Course = await getCourseById(id);
+    responseBody = []
+    if (Course) {
+        student = await getStudentByCourseId(id);
+
+        for (i = 0; i < student.length; i++) {
+            id = student[i]._id
+            responseBody.push(`id: ${id}`)
+        }
+    }
+    Course.students = responseBody
+    return Course;
+}
+exports.getCourseStudentsById = getCourseStudentsById;
+
+
+async function getCourseAssignmentsById(id) {
+    /*
+     * Execute three sequential queries to get all of the info about the
+     * specified Course, including its photos.
+     */
+    const Course = await getCourseById(id);
+    responseBody = []
+    if (Course) {
+        assignment = await getAssignmentByCourseId(id);
+
+        for (i = 0; i < assignment.length; i++) {
+            id = assignment[i]._id
+            responseBody.push(`id: ${id}`)
+        }
+    }
+    Course.assignments = responseBody
+    return Course;
+}
+exports.getCourseAssignmentsById = getCourseAssignmentsById;
+
+async function getCourseById(id) {
+    const db = getDBReference();
+    const collection = db.collection('Courses');
+    if (!ObjectId.isValid(id)) {
+        return null;
+    } else {
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .toArray();
+        return results[0];
+    }
+}
+exports.getCourseById = getCourseById;
+
+async function deleteCourseById(id) {
+    const db = getDBReference();
+    const collection = db.collection('Courses');
+    const result = await collection.deleteOne({
+        _id: new ObjectId(id)
+
+    });
+    return result.deletedCount > 0;
+}
+exports.deleteCourseById = deleteCourseById;
